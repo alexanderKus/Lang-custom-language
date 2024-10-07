d41ffc04ad3c8b086a53ef25021831e57d6fa597
Aleksander Kuś <79520681+alexanderKus@users.noreply.github.com>
2024-10-07 21:37:57 +0200

    Update resolver.py

--- python/resolver.py
+++ python/resolver.py
@@ -4,15 +4,6 @@ from common import Visitor
 class Resolver(Visitor):
     def __init__(self, interpreter):
         self.interpreter = interpreter
-    
-    def visit_block_stmt(self, stmt):
-        self.begin_scope()
-        self.resolve(stmt.statements)
-        self.end_scope()
-        return None
-
-    def begin_scope(self):
-        pass
 
     def resolve(self, stmts):
         for stmt in stmts:
@@ -21,5 +12,66 @@ class Resolver(Visitor):
     def _resolve(self, stmt):
         stmt.accept(self)
 
+    def begin_scope(self):
+        pass
+
     def end_scope(self):
+        pass
+
+    def visit_function_stmt(self, stmt):
+        pass
+
+    def visit_if_stmt(self, stmt):
+        pass
+
+    def visit_var_stmt(self, stmt):
+        pass
+
+    def visit_expression_stmt(self, stmt):
+        pass
+
+    def visit_print_stmt(self, stmt):
+        pass
+
+    def visit_return_stmt(self, stmt):
+        pass
+
+    def visit_while_stmt(self, stmt):
+        pass
+
+    def visit_block_stmt(self, stmt):
+        self.begin_scope()
+        self.resolve(stmt.statements)
+        self.end_scope()
+        return None
+
+    def visit_break_stmt(self, stmt):
+        pass
+
+    # Expressions
+    def visit_function_expr(self, expr):
+        pass
+    
+    def visit_logical_expr(self, expr):
+        pass
+
+    def visit_call_expr(self, expr):
+        pass
+
+    def visit_variable_expr(self, expr):
+        pass
+
+    def visit_assign_expr(self, expr):
+        pass
+
+    def visit_binary_expr(self, expr):
+        pass
+
+    def visit_grouping_expr(self, expr):
+        pass
+
+    def visit_literal_expr(self, expr):
+        pass
+
+    def visit_unary_expr(self, expr):
         pass
