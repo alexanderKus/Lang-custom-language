e20c6743c4a906557b6daa6061dbcb6571e457c9
Aleksander Kuś <79520681+alexanderKus@users.noreply.github.com>
2024-10-13 15:12:06 +0200

    Update examples and run_examples_test.sh

--- python/run_examples_test.sh
+++ python/run_examples_test.sh
@@ -8,7 +8,12 @@ do
   if [ "$result" -eq 0 ]
   then
     echo -e "$entry \033[32mPASSED...\033[0m"
-  else
+  else 
+    if [ "$entry" == "../examples/blocks_funny.lang" ]
+    then
+      echo -e "$entry \033[31mFALIED...\033[0m - and should have failed"
+      continue
+    fi
     echo -e "$entry \033[31mFALIED...\033[0m"
   fi
 done
